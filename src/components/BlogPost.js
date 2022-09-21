121371f080f735055c10d9cb3dc23e77c0d8e860
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-09-21 05:27:40 +0300

    completed react hooks props-basics assignment

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -5,10 +5,13 @@ import Comment from "./Comment";
 function BlogPost() {
   return (
     <div id="blog-post">
-      <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
+      <BlogContent articleText="I know Joash is a top notch Software Engineer"
+        isPublished={true} 
+        forHowLong="8 months"/>
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
       <Comment commentText="Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451" />
+      <Comment commentText="I know this to be true. - Bill Gates" />
     </div>
   );
 }
